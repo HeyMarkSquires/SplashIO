e2a8f612b6ef80c7c966583d0dc04df312c52e8d
MrRenma <mjsquires47@gmail.com>
2020-02-15 17:01:06 +0000

    Ultrasonics now read to two decimal places

--- Scan.py
+++ Scan.py
@@ -2,10 +2,12 @@ import serial
 from time import sleep
 import numpy as np
 import tensorflow as tf
+import keyboard as kb
 
 def parser(data):
-    t=data[:-2]
-    x=list(map(int, t.split()))
+    #t=data[:-2]
+    sList=data.split()
+    x=list(map(float, sList))
     result=np.array(x)
     return result
 
@@ -13,18 +15,21 @@ def parser(data):
 ser = serial.Serial('COM9', 9600)
 val=ser.readline().decode("utf-8")
 state=np.zeros((20,4))
-print(state)
 count=0
 #Reading information from the sensors and generating a numpy array to determine
 #the state of the pool
 while True:
-    g=ser.readline().decode("utf-8")
-    x=parser(g)
+    if kb.is_pressed('q'):
+        print("Hello")
+    x=parser(val)
+    print(x)
+    '''
     state[count]=x
     print(x)
     if count<20:    
         count+=1
     else:
+        print(state)
         count=0
-    sleep(.1)
+    sleep(.1)'''
     
---
+++ UltraRead/UltraRead.ino
@@ -0,0 +1,99 @@
+  
+const int trigPin1=2;
+const int echoPin1=3;
+const int trigPin2=4;
+const int echoPin2=5;
+const int trigPin3=6;
+const int echoPin3=7;
+const int trigPin4=8;
+const int echoPin4=9;
+long duration1;
+float distance1;
+long duration2;
+float distance2;
+long duration3;
+float distance3;
+long duration4;
+float distance4;
+void setup() {
+  pinMode(trigPin1, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin1, INPUT); // Sets the echoPin as an Input
+  pinMode(trigPin2, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin2, INPUT); // Sets the echoPin as an Input
+  pinMode(trigPin3, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin3, INPUT); // Sets the echoPin as an Input
+  pinMode(trigPin4, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin4, INPUT); // Sets the echoPin as an Input
+  Serial.begin(9600); // Starts the serial communication
+}
+void loop() {
+  ultra1();
+  Serial.print(" ");
+  ultra2();
+  Serial.print(" ");
+  ultra3();
+  Serial.print(" ");
+  ultra4();
+  Serial.println("");
+  delay(2000);
+}
+
+void ultra1(){
+  digitalWrite(trigPin1, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin1, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin1, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration1 = pulseIn(echoPin1, HIGH);
+  // Calculating the distance
+  distance1= duration1*0.034/2;
+  // Prints the distance on the Serial Monitor
+  Serial.print(distance1);
+}
+
+void ultra2(){
+  digitalWrite(trigPin2, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin2, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin2, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration2 = pulseIn(echoPin2, HIGH);
+  // Calculating the distance
+  distance2= duration2*0.034/2;
+  // Prints the distance on the Serial Monitor
+  Serial.print(distance2);
+}
+
+void ultra3(){
+  digitalWrite(trigPin3, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin3, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin3, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration3 = pulseIn(echoPin3, HIGH);
+  // Calculating the distance
+  distance3= duration3*0.034/2;
+  // Prints the distance on the Serial Monitor
+  Serial.print(distance3);
+}
+
+void ultra4(){
+  digitalWrite(trigPin4, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin4, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin4, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration4 = pulseIn(echoPin4, HIGH);
+  // Calculating the distance
+  distance4= duration4*0.034/2;
+  // Prints the distance on the Serial Monitor
+  Serial.print(distance4);
+}
