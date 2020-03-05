727674d24fa1e9ca7fa48fa7e72cdda8264893d4
MrRenma <mjsquires47@gmail.com>
2020-03-05 09:04:12 +0000

    Data recording is now triggered and carried out correctly

--- Scan.py
+++ Scan.py
@@ -9,6 +9,14 @@ import keyboard as kb
 from numpy import asarray
 from numpy import savetxt
 import csv
+
+def parser(data):
+    sList=data.split()
+    x=list(map(float, sList))
+    result=np.array(x)
+    return result
+
+
 #Checking if data.csv already exists. If it doesn't, initialise the headers
 headers=["type", "1sen1", "2sen1", "3sen1", "4sen1",  "1Sen2", "2sen2", "3sen2", "4sen2",  "1sen3", "2sen3", "3sen3", "4sen3",
          "1sen4", "2sen4", "3sen4", "4sen4",  "1Sen5", "2sen5", "3sen5", "4sen5",  "1sen6", "2sen6", "3sen6", "4sen6",
@@ -17,11 +25,7 @@ headers=["type", "1sen1", "2sen1", "3sen1", "4sen1",  "1Sen2", "2sen2", "3sen2",
          "1sen13", "2sen13", "3sen13", "4sen13",  "1sen14", "2sen14", "3sen14", "4sen14",  "1sen15", "2sen15", "3sen15", "4sen15",
          "1sen16", "2sen16", "3sen16", "4sen16",  "1sen17", "2sen17", "3sen17", "4sen17",  "1sen18", "2sen18", "3sen18", "4sen18",
          "1sen19", "2sen19", "3sen19", "4sen19",  "1sen20", "2sen20", "3sen20", "4sen20"]
-def parser(data):
-    sList=data.split()
-    x=list(map(float, sList))
-    result=np.array(x)
-    return result
+
 try:
     f=open("data.csv")
 except IOError:
@@ -41,28 +45,35 @@ with open('data.csv', 'a', newline='') as file:
     wr=csv.writer(file, quoting=csv.QUOTE_ALL)
 #Reading information from the sensors and generating a numpy array to determine
 #the state of the pool
+measuring=False
 while True:
     if kb.is_pressed('q'):
-        print("Hello")
-         x=parser(val)
-        val=ser.readline().decode("utf-8")
-        state[count]=x
-        if count<19:    
-            count+=1
-        else:
-            g=state.flatten()
-            count=0
-            #Creating the array that will be written to the csv file
-            newType=int(input("Action number: \n"))
-            print(newType)
-            r=[newType]
-            for i in range(len(g)):
-                r.append(g[i])
-            #Writing the file to a csv
-            print(r)
-            with open('data.csv', 'a', newline='') as file:
-                      wr=csv.writer(file, quoting=csv.QUOTE_ALL)
-                      wr.writerow(r)
-        sleep(.1)
+        measuring=True
+        while measuring==True:
+            print(count)
+            x=parser(val)
+            val=ser.readline().decode("utf-8")
+            state[count]=x
+            if count<19:
+                print(count)
+                count+=1
+            else:
+                g=state.flatten()
+                print("Loop complete")
+                count=0
+                #Creating the array that will be written to the csv file
+                newType=int(input("Action number: \n"))
+                print(newType)
+                r=[newType]
+                for i in range(len(g)):
+                    r.append(g[i])
+                #Writing the file to a csv
+                print(r)
+                with open('data.csv', 'a', newline='') as file:
+                          wr=csv.writer(file, quoting=csv.QUOTE_ALL)
+                          wr.writerow(r)
+                measuring=False
+            sleep(.1)
+            
    
     
